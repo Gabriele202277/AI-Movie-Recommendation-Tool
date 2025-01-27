ae837445d1336971b069ca11456bb3fde50fee5f
Gabriele202277 <s202277@student.dtu.dk>
2025-01-27 12:26:02 +0100

    Update Movies_Recommender.py

--- Movies_Recommender.py
+++ Movies_Recommender.py
@@ -8,7 +8,7 @@ Created on Sun Jan 26 18:55:44 2025
 import pandas as pd
 import matplotlib.pyplot as plt
 
-save_json = True
+
 
 ## Part 1: read csv files and visualization
 
@@ -22,6 +22,57 @@ print(movies_metadata.head())
 print(ratings.head())
 
 
-if save_json: 
-    movies_metadata.head().to_json("movies_metadata_head.json", orient="records", indent=4)
-    ratings.head().to_json("ratings_head.json", orient="records", indent=4)
+# Count how many unique movies there are
+unique_movies = movies_metadata['movie_id'].nunique()
+
+# Count how many unique users have rated how many unique movies
+unique_users = ratings['user_id'].nunique()
+unique_rated_movies = ratings['movie_id'].nunique()
+
+print(f"Number of unique movies: {unique_movies}")
+print(f"Number of unique users: {unique_users}")
+print(f"Number of unique rated movies: {unique_rated_movies}")
+
+
+
+# Visualise the vote_average column
+plt.figure(figsize=(10, 5))
+plt.hist(movies_metadata['vote_average'].dropna(), bins=30, edgecolor='k', alpha=0.7)
+plt.title('Distribution of Vote Average (movies_metadata.csv)', fontsize=16)
+plt.xlabel('Vote Average', fontsize=16)
+plt.ylabel('Frequency', fontsize=16)
+plt.grid(True)
+plt.show()
+
+# Visualise the vote_count column
+plt.figure(figsize=(10, 5))
+plt.hist(movies_metadata['vote_count'].dropna(), bins=30, edgecolor='k', alpha=0.7)
+plt.title('Distribution of Votes Count / movie (movies_metadata.csv)', fontsize=16)
+plt.xlabel('Vote Count', fontsize=16)
+plt.ylabel('Frequency', fontsize=16)
+plt.grid(True)
+plt.xlim(0, 8000)
+plt.show()
+
+
+# Visualise the distribution of the rating column
+plt.figure(figsize=(10, 5))
+plt.hist(ratings['rating'].dropna(), bins=10, edgecolor='k', alpha=0.7)
+plt.title('Distribution of Ratings (ratings.csv)', fontsize=16)
+plt.xlabel('Rating', fontsize=16)
+plt.ylabel('Frequency', fontsize=16)
+plt.grid(True)
+plt.show()
+
+# Visualise the ratings count
+
+plt.figure(figsize=(10, 5))
+plt.hist(ratings['movie_id'].value_counts(), bins=30, edgecolor='k', alpha=0.7)
+plt.title('Distribution of Ratings Count / movie (ratings.csv)', fontsize=16)
+plt.ylabel('Frequency', fontsize=16)
+plt.xlabel('Ratings count', fontsize=16)
+plt.grid(True)
+plt.xlim(0, 250)
+plt.show()
+
+
